0e9a7b0722450b5df1faa43790d2b22b4f2817bb
kaikyinho <kaikysilvadealmeida7@gmail.com>
2024-12-05 12:52:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'matematica',
+    'qual é a operação contrária da exponenciação?',
+    'é a radiciação'
+)
+
+criaCartao(
+    'Geografia',
+    'em qual continente fica seychelles?',
+    'seychelles fica na africa'
+)
+
+criaCartao(
+    'matematica',
+    'O que é uma função exponencial?',
+    'Função Exponencial é aquela que a variável está no expoente e cuja base é sempre maior que zero e diferente de um.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz carregador em Inglês?',
+    'carregador em ingles é charger'
+)
